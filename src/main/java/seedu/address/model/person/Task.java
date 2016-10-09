0fe8332ed8aa69b9361391ab1c8746df72e415e9
vitquay1996 <vitquay1996@gmail.com>
2016-10-09 23:16:05 +0800

    add Task

--- src/main/java/seedu/address/model/person/Task.java
+++ src/main/java/seedu/address/model/person/Task.java
@@ -1,5 +1,83 @@
 package seedu.address.model.person;
 
-public class Task {
+import java.util.Objects;
+
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.model.tag.UniqueTagList;
+
+/**
+ * Represents a task in Hard2Do.
+ * Guarantees: details are present and not null, field values are validated.
+ */
+public class Task implements ReadOnlyTask {
+
+    private Content content;
+    private TaskDate date;
+    private TaskTime time;
+
+    private UniqueTagList tags;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Task(Content content, TaskDate date, TaskTime time, UniqueTagList tags) {
+        assert !CollectionUtil.isAnyNull(content, date, time, tags);
+        this.content = content;
+        this.date = date;
+        this.time = time;
+        this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+    }
+
+    /**
+     * Copy constructor.
+     */
+    public Task(ReadOnlyTask source) {
+        this(source.getContent(), source.getDate(), source.getTime(), source.getTags());
+    }
+
+    @Override
+    public UniqueTagList getTags() {
+        return new UniqueTagList(tags);
+    }
+
+    /**
+     * Replaces this task's tags with the tags in the argument tag list.
+     */
+    public void setTags(UniqueTagList replacement) {
+        tags.setTags(replacement);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ReadOnlyTask // instanceof handles nulls
+                && this.isSameStateAs((ReadOnlyTask) other));
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(content, date, time, tags);
+    }
+
+    @Override
+    public String toString() {
+        return getAsText();
+    }
+
+    @Override
+    public Content getContent() {
+        return content;
+    }
+
+    @Override
+    public TaskDate getDate() {
+        return date;
+    }
+
+    @Override
+    public TaskTime getTime() {
+        return time;
+    }
 
 }
