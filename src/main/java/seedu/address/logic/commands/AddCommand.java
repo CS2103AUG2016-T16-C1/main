53ac727059df88147eb1954767ad84641d62d927
Wei Jie <ngweijie94@gmail.com>
2016-10-12 14:52:48 +0800

    modified parser, addcommand, taskdate, to suit regex for date

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -17,9 +17,9 @@ public class AddCommand extends Command {
     public static final String COMMAND_WORD = "add";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to the task manager. "
-            + "Parameters: c/CONTENT d/DATE[dd/mm/yyyy] t/time[HH:mm] [#TAG]...\n"
+            + "Parameters: c/CONTENT d/DATE[dd-mm-yyyy] t/time[HH:mm] [#TAG]...\n"
             + "Example: " + COMMAND_WORD
-            + " c/do this task manager d/20/10/2016 t/1300 #shaglife #wheregottime";
+            + " c/do this task manager d/20/10/2016 t/13:00 #shaglife #wheregottime";
 
     public static final String MESSAGE_SUCCESS = "New task added: %1$s";
     public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the task manager";
