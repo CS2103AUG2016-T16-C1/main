d8321432fbfb4f4bca8a25a722d7255c946ddebe
vitquay1996 <vitquay1996@gmail.com>
2016-10-16 16:26:36 +0800

    successfully initiate the detail pane

--- src/main/java/seedu/address/ui/TaskDetail.java
+++ src/main/java/seedu/address/ui/TaskDetail.java
@@ -4,13 +4,20 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import seedu.address.model.person.ReadOnlyTask;
 
 public class TaskDetail extends UiPart {
-    
+
     private static final String FXML = "TaskDetail.fxml";
     
+    private AnchorPane placeHolderPane;
+    private AnchorPane taskDetailPane;
+    
+    @FXML
+    private VBox detailView;
+
     @FXML
     private Label content;
     @FXML
@@ -19,36 +26,38 @@ public class TaskDetail extends UiPart {
     private Label tags;
     @FXML
     private Label date;
-    
-    private ReadOnlyTask task;
 
     public TaskDetail() {
     }
-    
 
-    public static TaskDetail load(Stage primaryStage, AnchorPane placeHolder){
+    public static TaskDetail load(Stage primaryStage, AnchorPane placeHolder) {
+        System.out.println("TaskDetail is called");
         TaskDetail detail = UiPartLoader.loadUiPart(primaryStage, placeHolder, new TaskDetail());
+        detail.addToPlaceHolder();
         return detail;
     }
-    
-    public void loadTaskDetail(ReadOnlyTask task) {
-        this.task = task;
-        this.initialize();
-    }
 
-    @FXML
-    public void initialize() {
+    public void loadTaskDetail(ReadOnlyTask task) {
         content.setText(task.getContent().toString());
         date.setText(task.getDate().toString());
         time.setText(task.getTime().toString());
 
-        tags.setText(task.tagsString());
+        tags.setText(task.tagsString());    
+    }
+    
+    private void addToPlaceHolder() {
+        placeHolderPane.getChildren().add(detailView);
+    }
+
+
+    @Override
+    public void setNode(Node node) {
+        taskDetailPane = (AnchorPane) node;
     }
     
     @Override
-    public void setNode(Node node) {
-        // TODO Auto-generated method stub
-
+    public void setPlaceholder(AnchorPane pane) {
+        this.placeHolderPane = pane;
     }
 
     @Override
