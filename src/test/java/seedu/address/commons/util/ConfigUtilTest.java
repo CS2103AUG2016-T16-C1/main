f71c60981f41a6f80533d41a702bd36361f7f2df
vitquay1996 <vitquay1996@gmail.com>
2016-10-22 00:57:30 +0800

    xmlUtilTest works, configUtil does not

--- src/test/java/seedu/address/commons/util/ConfigUtilTest.java
+++ src/test/java/seedu/address/commons/util/ConfigUtilTest.java
@@ -73,7 +73,7 @@ public class ConfigUtilTest {
 
     private Config getTypicalConfig() {
         Config config = new Config();
-        config.setAppTitle("Typical App Title");
+        config.setAppTitle("Hard2Do");
         config.setLogLevel(Level.INFO);
         config.setUserPrefsFilePath("preferences.json");
         config.setTaskManagerFilePath("data/taskmanager.xml");
