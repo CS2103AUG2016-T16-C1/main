719ff4bc342a76a14c1593373e49f88f29484a4c
vitquay1996 <vitquay1996@gmail.com>
2016-10-22 12:47:19 +0800

    change TaskCardHandle

--- src/test/java/guitests/guihandles/TaskCardHandle.java
+++ src/test/java/guitests/guihandles/TaskCardHandle.java
@@ -11,7 +11,6 @@ import seedu.address.model.person.ReadOnlyTask;
 public class TaskCardHandle extends GuiHandle {
     private static final String CONTENT_FIELD_ID = "#content";
     private static final String DATE_FIELD_ID = "#date";
-    private static final String TIME_FIELD_ID = "#time";
 
     private Node node;
 
@@ -32,9 +31,7 @@ public class TaskCardHandle extends GuiHandle {
         return getTextFromLabel(DATE_FIELD_ID);
     }
 
-    public String getTime() {
-        return getTextFromLabel(TIME_FIELD_ID);
-    }
+
 
     public boolean isSameTask(ReadOnlyTask task){
         return getContent().toString().equals(task.getContent().value) && getDate().toString().equals(task.getDate().toString());
@@ -45,14 +42,13 @@ public class TaskCardHandle extends GuiHandle {
         if(obj instanceof TaskCardHandle) {
             TaskCardHandle handle = (TaskCardHandle) obj;
             return getContent().equals(handle.getContent())
-                    && getDate().equals(handle.getDate())
-                    && getTime().equals(handle.getTime());
+                    && getDate().equals(handle.getDate());
         }
         return super.equals(obj);
     }
 
     @Override
     public String toString() {
-        return getContent() + " " + getDate() + " " + getTime();
+        return getContent() + " " + getDate();
     }
 }
