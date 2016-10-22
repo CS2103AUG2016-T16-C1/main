a4bca78b0d7464fdb4013f18eadef434958d37ed
vitquay1996 <vitquay1996@gmail.com>
2016-10-22 12:50:24 +0800

    nothing much

--- src/test/java/guitests/guihandles/TaskDetailHandle.java
+++ src/test/java/guitests/guihandles/TaskDetailHandle.java
@@ -1,5 +1,19 @@
 package guitests.guihandles;
 
-public class TaskDetailHandle {
+import guitests.GuiRobot;
+import javafx.stage.Stage;
+
+public class TaskDetailHandle extends GuiHandle {
+    private static final String CONTENT_FIELD_ID = "#content";
+    private static final String DATE_FIELD_ID = "#datePicker";
+    private static final String Time_FIELD_ID = "#timePicker";
+    
+
+    
+    public TaskDetailHandle(GuiRobot guiRobot, Stage primaryStage, String stageTitle) {
+        super(guiRobot, primaryStage, stageTitle);
+    }
+    
+    
 
 }
