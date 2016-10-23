1134d1492fac470abe08fb812fcac7cd870905be
vitquay1996 <vitquay1996@gmail.com>
2016-10-23 21:44:10 +0800

    change for logic manager

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -221,7 +221,7 @@ public class LogicManagerTest {
         helper.addToModel(model, 2);
 
         assertCommandBehavior("list",
-                ListCommand.MESSAGE_SUCCESS,
+                ListCommand.MESSAGE_SUCCESS_ALL,
                 expectedTM,
                 expectedList);
     }
